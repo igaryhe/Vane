0fdb0a01ae53491fc7b57bd2c2d6e90c52f75bf9
Dan Ho <igaryhe@gmail.com>
2019-07-28 11:50:18 +0800

    New camera control.

---
+++ Assets/CameraRotator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    private float speed = 0.01f;
+    private float angleOffset = 45;
+    Vector3 lastMousePos;
+    private float minSize = 2f;
+    private float maxSize = 7.5f;
+    private readonly float _overTime = .5f;
+    private bool _running;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && Camera.main.orthographicSize < maxSize)
+        {
+            Camera.main.orthographicSize += 0.5f;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && Camera.main.orthographicSize > minSize)
+        {
+            Camera.main.orthographicSize -= 0.5f;
+        }
+        if (Input.GetMouseButton(2))
+        {
+            if(lastMousePos != Vector3.zero)
+            {
+                Vector3 offset = (lastMousePos - Input.mousePosition) * speed;
+                offset = Quaternion.AngleAxis(angleOffset, Vector3.forward) * offset;
+                transform.position += new Vector3(offset.x, 0, offset.y);
+            }
+        }
+        if (Input.GetMouseButtonUp(2))
+        {
+            lastMousePos = Vector3.zero;
+        }
+        lastMousePos = Input.mousePosition;
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (_running) return;
+            StartCoroutine(Rotate(90));
+            // transform.Rotate(Vector3.up, 90, Space.World);
+            angleOffset -= 90;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (_running) return;
+            StartCoroutine(Rotate(-90));
+            // transform.Rotate(Vector3.up, -90, Space.World);
+            angleOffset += 90;
+        }
+    }
+    
+    private IEnumerator Rotate(float degree)
+    {
+        _running = true;
+        var rotation = Quaternion.Euler(0, degree, 0);
+        var postRotation = transform.rotation * rotation;
+        var startTime = Time.time;
+        while (Time.time < startTime + _overTime)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, postRotation, 
+                (Time.time - startTime) / _overTime);
+            yield return null;
+        }
+        transform.rotation = postRotation;
+        _running = false;
+    }
+}
