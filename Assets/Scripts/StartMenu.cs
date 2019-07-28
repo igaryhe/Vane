b6f2d075261427e040ca78f01eba3af5e4cfb17b
Dan Ho <igaryhe@gmail.com>
2019-07-28 17:23:05 +0800

    Start menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Selection");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
