dd6bb7e900bbc95e7d616e826221d388c16db176
Dan Ho <igaryhe@gmail.com>
2019-07-29 19:12:42 +0800

    Start menu background scroll

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    private float speed = -0.05f;
+
+    private Material _mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mat.mainTextureOffset = new Vector2(Time.time * speed, 0f);
+    }
+}
